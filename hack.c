fb8fccd4bd09a90ef27841b0146574107e7780fa
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-01-24 10:14:53 -0500

    first commit

---
+++ hack.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef char * string;
+int main(int argc, string argv[])]
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <website-url>\n", argv[0]);
+        return 1;
+    }
+    return 0;
+}
+
